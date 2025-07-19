fda2988e733615282f140d682b921f428cf846c2
luschee <lukasscheerer89@gmail.com>
2025-07-19 19:55:53 +0200

    Log errors

--- jitpack.sh
+++ jitpack.sh
@@ -10,6 +10,12 @@ export ANDROID_NDK_ROOT=$HOME/android-ndk
 export ANDROID_NDK_HOME=$HOME/android-ndk
 export ANDROID_NDK=$HOME/android-ndk
 
+printenv
+
 cd ffmpeg-kit
 chmod +x android.sh
-./android.sh --enable-gnutls --disable-x86 --disable-x86-64 --disable-arm-v7a --api-level=21
+./android.sh --enable-gnutls --disable-x86 --disable-x86-64 --disable-arm-v7a --api-level=21 || export BUILD_FAILED=true
+echo "======= BEGIN build.log (arm-v7a-neon) ======="
+tail -n 200 build.log || echo "build.log not found"
+echo "======= END build.log ======="
+if [ "$BUILD_FAILED" = "true" ]; then exit 1; fi
